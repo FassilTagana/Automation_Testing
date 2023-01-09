ce920915f2658291d52b208ce4ce022986c424ed
Fassil Tagana <122093690+FassilTagana100@users.noreply.github.com>
2023-01-09 15:56:12 +0200

    Add files via upload

---
+++ Page_Elements.py
@@ -0,0 +1,58 @@
+""" This document contains all the project page Elements (By xpath/ID/Name) """
+# -----------------------------------------------------------------------------------------
+
+# Page URL:
+Page_URl = 'https://demo.nopcommerce.com/'
+
+# Sign In Test:
+login_button_by_xpath = '/html/body/div[6]/div[1]/div[1]/div[2]/div[1]/ul/li[2]/a'
+register_button_by_xpath = '/html/body/div[6]/div[1]/div[1]/div[2]/div[1]/ul/li[1]/a'
+# register_button_by_xpath = '/html/body/div[6]/div[3]/div/div/div/div[2]/div[1]/div[1]/div[3]/button'
+male_gender_button_by_xpath = '//*[@id="gender-male"]'
+first_name_by_xpath = '//*[@id="FirstName"]'
+first_name = 'Fassil'
+last_name_by_xpath = '//*[@id="LastName"]'
+last_name = 'Tagana'
+day_one_button_by_xpath = '/html/body/div[6]/div[3]/div/div/div/div[2]/form/div[1]/div[2]/div[4]/div/select[1]/option[2]'
+month_april_by_xpath = '/html/body/div[6]/div[3]/div/div/div/div[2]/form/div[1]/div[2]/div[4]/div/select[2]/option[5]'
+year_1998_by_xpath = '/html/body/div[6]/div[3]/div/div/div/div[2]/form/div[1]/div[2]/div[4]/div/select[3]/option[84]'
+email_by_xpath = '//*[@id="Email"]'
+email = 'FassilTagana101@gmail.com'
+company_name_by_xpath = '//*[@id="Company"]'
+company_name = 'Google'
+password_by_xpath = '//*[@id="Password"]'
+password = 'q1w2e3r4'
+confirm_password_by_xpath = '//*[@id="ConfirmPassword"]'
+register_second_button_by_xpath = '//*[@id="register-button"]'
+
+# Login section:
+log_in_second_button_by_xpath = '/html/body/div[6]/div[3]/div/div/div/div[2]/div[1]/div[2]/form/div[3]/button'
+
+# Login + porches items:
+computers_tab_button_by_xpath = '/html/body/div[6]/div[2]/ul[1]/li[1]/a'
+desktops_button_by_xpath = '/html/body/div[6]/div[3]/div/div[3]/div/div[2]/div[1]/div/div[1]/div/div/a/img'
+add_to_cart1_button_by_xpath = '/html/body/div[6]/div[3]/div/div[3]/div/div[2]/div[2]/div[2]/div/div/div[3]/div/div[2]/div[3]/div[2]/button[1]'
+add_to_cart2_button_by_xpath = '/html/body/div[6]/div[3]/div/div[3]/div/div[2]/div[2]/div[2]/div/div/div[2]/div/div[2]/div[3]/div[2]/button[1]'
+x_button_pop_up_by_xpath = '//*[@id="bar-notification"]/div/span'
+shopping_cart_button_by_xpath = '//*[@id="topcartlink"]/a/span[1]'
+checkout_button_by_xpath = '/html/body/div[6]/div[3]/div/div/div/div[2]/div/form/div[3]/div[2]/div[4]/button'
+x_button_terms_by_xpath = '/html/body/div[8]/div[1]/button'
+checkbox_term_by_id = 'termsofservice'
+
+# Shipping information:
+israel_country_by_xpath = '//*[@id="BillingNewAddress_CountryId"]/option[106]'
+city_by_xpath = '//*[@id="BillingNewAddress_City"]'
+city = 'Haifa'
+address1_by_xpath = '//*[@id="BillingNewAddress_Address1"]'
+address1 = 'Hardofim 3/1'
+zip_code_by_xpath = '//*[@id="BillingNewAddress_ZipPostalCode"]'
+zip_code = '20692'
+phone_number_by_xpath = '//*[@id="BillingNewAddress_PhoneNumber"]'
+phone_number = '0549293393'
+continue_button1_by_xpath = '//*[@id="billing-buttons-container"]/button[4]'
+continue_button2_by_xpath = '//*[@id="shipping-method-buttons-container"]/button'
+continue_button3_by_xpath = '//*[@id="payment-method-buttons-container"]/button'
+continue_button4_by_xpath = '//*[@id="payment-info-buttons-container"]/button'
+confirm_button_by_xpath = '//*[@id="confirm-order-buttons-container"]/button'
+continue_button5_by_xpath = '//*[@id="payment-info-buttons-container"]/button'
+# ----------------------------------------------------------------------------------------------------
